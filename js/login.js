9f5867590adaee85b452fd54a2471a8ae65eb488
AnanthaVarma <ananthavarma.tech@gmail.com>
2019-06-20 22:25:51 +0530

    login page bug fix when userdata is null

--- js/login.js
+++ js/login.js
@@ -3,11 +3,10 @@ function userLogin(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value; 
     var userdata = JSON.parse (localStorage.getItem(username)) ;
-        // console.log(userdata);
-    if((userdata.username == username) && (userdata.password == password)){
-        location.replace("home.html")
+         console.log(userdata);
+    if( (userdata != null) && ((userdata.username == username) && (userdata.password == password)) ){
+        location.replace("../index.html")
     }else{
         document.getElementById("msg").innerHTML = "Check your User name and Password ...!";
     }
-        
 }
